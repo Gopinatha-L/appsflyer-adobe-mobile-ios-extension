4a987c03212695d517762f649bd03825e031a055
gopinathan.l <gopi36.ako@gmail.com>
2022-02-21 21:13:36 +0530

    remove AEPCore dep

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework", from: "6.4.4"),
-        .package(url: "https://github.com/adobe/aepsdk-core-ios.git", .upToNextMajor(from: "3.0.0"))
+//        .package(url: "https://github.com/adobe/aepsdk-core-ios.git", .upToNextMajor(from: "3.0.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -23,7 +23,7 @@ let package = Package(
         .target(
             name: "AppsFlyerAdobeExtension",
             dependencies: [
-                .product(name: "AEPCore", package: "aepsdk-core-ios"),
+//                .product(name: "AEPCore", package: "aepsdk-core-ios"),
                 .product(name: "AppsFlyerLib", package:"AppsFlyerFramework")
 
             ],
